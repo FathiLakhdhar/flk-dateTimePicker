5cd6c09461089811a7292c242e21f6ada25c4797
FathiLakhdhar <fathi.lakdhar@yahoo.fr>
2018-12-29 14:34:57 +0100

    201812291434 : test datepicker by creating random (start day, end day, holidays)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  public startDate = "2018-12-26";
-  public endDate = "2019-01-20";
-  public holidays = ["2018-12-25", "2018-12-28", "2019-01-13"];
-  public weekHolidays = [0, 6];
+export class AppComponent implements OnInit{
+  public startDate : string;
+  public endDate : string;
+  public holidays : string[] = [];
+  public weekHolidays = [0, 6];//0..6, 0 -> Sunday | 6 -> Saturday
+  ngOnInit(){
+    let today = new Date();
+    this.startDate = (new Date((new Date()).setDate(today.getDate() - Math.floor(Math.random() * 7)))).toISOString().split("T")[0];
+    this.endDate = (new Date((new Date()).setDate(today.getDate() + 20 + Math.floor(Math.random() * 20)))).toISOString().split("T")[0];
+    // random 3 holidays [today+1 .. today + 20]
+    while(this.holidays.length < 3){
+      let randomHoliday = (new Date( (new Date()).setDate(today.getDate() + 1 + Math.floor(Math.random() * 20)))).toISOString().split("T")[0];
+      if(!this.holidays.includes(randomHoliday)) { this.holidays.push(randomHoliday) }
+    }
+  }
   select(event : any){
     console.log("Selected : ", event);
   }
